fee422a70514f3101c3c19e9b4b1e6536ecb5cde
Jeroen Vegter <jeroenvegter@hotmail.com>
2019-05-12 21:08:02 +0000

    changed to Europe and added countrylist selector for European countries

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+var map, places, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0.8072863, lng: -176.6264345},
+    zoom: 2
+  });
+}
+
+infoWindow = new google.maps.InfoWindow({
+          content: document.getElementById('info-content')
         });
-      }
+        
+ var autocomplete = new google.maps.places.Autocomplete(input);
+     autocomplete.bindTo('bounds', map);
